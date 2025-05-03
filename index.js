8cf7d083cceda4020f04394634074095497fcef4
asifmahmud1047 <asif.mahmud1047@gmail.com>
2025-05-03 19:52:46 +0600

    delete section added

--- index.js
+++ index.js
@@ -69,6 +69,17 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/visa/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      try {
+        const result = await visaColection.deleteOne(query);
+        res.send(result);
+      } catch (error) {
+        res.status(500).send({ message: "Failed to delete visa", error });
+      }
+    });
+
     app.get("/apply", async (req, res) => {
       const data = applyColection.find();
       const result = await data.toArray();
